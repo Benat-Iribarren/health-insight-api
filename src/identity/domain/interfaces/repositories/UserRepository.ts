47f55faff7ea0b02c7ba8a6c5da900b45ec177c5
Beñat <150382660+Benat-Iribarren@users.noreply.github.com>
2025-12-31 20:10:23 +0100

    refactor identity

---
+++ src/identity/domain/interfaces/repositories/UserRepository.ts
@@ -0,0 +1,3 @@
+import { UserId } from '@common/domain/model/UserParameters';
+
+export interface UserRepository { isProfessional(userId: UserId): Promise<boolean>; isPatient(userId: UserId): Promise<boolean>; }
