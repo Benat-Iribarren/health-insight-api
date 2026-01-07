f302db1e85bbc603dd2e4daa79c640bd96c01262
Beñat <150382660+Benat-Iribarren@users.noreply.github.com>
2026-01-07 13:25:44 +0100

    Intento de correccion de smtp en produccion forzando un puerto valido en el envio de mensajes

--- src/messaging/infrastructure/smtp/SmtpMailRepository.ts
+++ src/messaging/infrastructure/smtp/SmtpMailRepository.ts
@@ -6,11 +6,14 @@ export class SmtpMailRepository implements MailRepository {
 
     constructor() {
         this.transporter = nodemailer.createTransport({
-            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASS,
             },
+            connectionTimeout: 10000
         });
     }
 
