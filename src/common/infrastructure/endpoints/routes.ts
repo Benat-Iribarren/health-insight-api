1b695845b14c17acba356cc1c35ddc28de31fe23
Beñat <150382660+Benat-Iribarren@users.noreply.github.com>
2026-01-14 16:47:32 +0100

    Arreglado routes para poder ejecutar el endpoint con el predictor de abandono

--- src/common/infrastructure/endpoints/routes.ts
+++ src/common/infrastructure/endpoints/routes.ts
@@ -1,44 +1,48 @@
 import { FastifyInstance } from 'fastify';
 import { supabaseClient } from '../database/supabaseClient';
-import { GmailApiMailRepository } from "@src/messaging/infrastructure/smtp/GmailApiMailRepository";
-import { SupabasePatientContactRepository } from '@src/messaging/infrastructure/database/SupabasePatientContactRepository';
-import { SupabaseStatsRepository } from '@src/messaging/infrastructure/database/SupabaseStatsRepository';
+import { authenticate } from '@src/identity/infrastructure/http/authenticate';
+import { SupabaseUserRepository } from '@src/identity/infrastructure/database/repositories/SupabaseUserRepository';
+import { verifyProfessional } from '@src/identity/infrastructure/http/verifyUser';
 
 import presenceMinute from '@src/biometrics/infrastructure/endpoints/presenceMinute';
-
+import predictDropout from '@src/clinical-intelligence/infrastructure/endpoints/predictDropout';
 import sendToPatient from '@src/messaging/infrastructure/endpoints/sendToPatient';
 import sendWeeklyStats from '@src/messaging/infrastructure/endpoints/sendWeeklyStats';
 
+import { SupabaseDropoutRepository } from '@src/clinical-intelligence/infrastructure/database/SupabaseDropoutRepository';
+import { SupabasePatientContactRepository } from '@src/messaging/infrastructure/database/SupabasePatientContactRepository';
+import { SupabaseStatsRepository } from '@src/messaging/infrastructure/database/SupabaseStatsRepository';
+import { GmailApiMailRepository } from "@src/messaging/infrastructure/smtp/GmailApiMailRepository";
+
 export function registerRoutes(fastify: FastifyInstance) {
+    const userRepo = new SupabaseUserRepository(supabaseClient);
+    const dropoutRepo = new SupabaseDropoutRepository(supabaseClient as any);
     const statsRepo = new SupabaseStatsRepository(supabaseClient);
     const patientContactRepo = new SupabasePatientContactRepository(supabaseClient);
     const mailRepo = new GmailApiMailRepository();
+
+    const isAuth = { preHandler: [authenticate] };
+    const isProfessional = { preHandler: [authenticate, verifyProfessional(userRepo)] };
 
     fastify.get('/ping', async () => {
         return { status: 'ok' };
     });
 
-    // 2. Registra el endpoint de presencia aquí.
-    // Al estar fuera de 'professionalApp', permites que los pacientes (autenticados)
-    // puedan enviar su heartbeat minutal.
     fastify.register(presenceMinute());
 
-    fastify.register(async (authenticatedApp) => {
-        // Los endpoints dentro de professionalApp requieren verificación extra
-        authenticatedApp.register(async (professionalApp) => {
-            professionalApp.register(
-                sendToPatient({
-                    patientContactRepo,
-                    mailRepo,
-                })
-            );
+    fastify.register(async (professionalApp) => {
+        // professionalApp.addHook('preHandler', authenticate);
 
-            professionalApp.register(
-                sendWeeklyStats({
-                    statsRepo,
-                    mailRepo,
-                })
-            );
-        });
+        professionalApp.register(predictDropout({ dropoutRepo }), isProfessional);
+
+        professionalApp.register(sendToPatient({
+            patientContactRepo,
+            mailRepo
+        }), isProfessional);
+
+        professionalApp.register(sendWeeklyStats({
+            statsRepo,
+            mailRepo
+        }), isProfessional);
     });
 }
